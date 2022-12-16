2443f4f1e4f43145d9fd9083eaac027c38db64ce
rahmanhakim7 <hakimrahman261@gmail.com>
2022-12-16 21:58:14 +0700

    Praktikum 8

---
+++ Praktikum8/MYDB.cs
@@ -0,0 +1,55 @@
+namespace Praktikum8
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using MySql.Data.MySqlClient;
+
+    public class MYDB : DbContext
+    {
+        // Your context has been configured to use a 'MYDB' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Praktikum8.MYDB' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'MYDB' 
+        // connection string in the application configuration file.
+        public MYDB()
+            : base("name=MYDB")
+        {
+        }
+
+        private MySqlConnection connection = new MySqlConnection(
+            "server=localhost;port=3306;username=root;password=;database=library_4416");
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
